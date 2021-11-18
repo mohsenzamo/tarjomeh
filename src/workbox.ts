7be0c1b02b731046e4e02564cb39ac2db23ecb5f
mohsen <mohsen.zahraee123445@gmail.com>
2021-11-18 16:00:15 +0330

    precache

--- src/workbox.ts
+++ src/workbox.ts
@@ -7,3 +7,6 @@ import { precacheAndRoute } from 'workbox-precaching'
 declare const self: ServiceWorkerGlobalScope
 
 precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute([
+  { url: '/assets/index.06f8855f.js', revision: 'null' },
+])  
